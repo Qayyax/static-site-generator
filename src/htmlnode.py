98f0b77e0593c79e03682a3e8c7082445728ff0a
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-03-08 17:09:36 -0500

    added a ParentNode child class, and test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -28,7 +28,7 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props)
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value is None:
@@ -40,3 +40,23 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
